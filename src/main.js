f9da803cd118d2405b8b652443eb4cd5a3ce33eb
allanbaque1510 <allanbaque1510@gmail.com>
2023-09-06 15:59:59 -0500

    Actualizando

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow  } = require('electron');
 const express = require('express');
 const puppeteer = require('puppeteer');
-
+const EventEmitter = require('events');
 const expressApp = express(); 
 expressApp.use(express.json());
+
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 900,
@@ -23,7 +25,7 @@ const coneccion = ()=>{
 const animeList = async (idBusqueda) => {
   const browser = await puppeteer.launch({headless:'new'})
   const page = await browser.newPage();
-  if(idBusqueda.includes('https://jkanime.net/buscar/')){
+  if(idBusqueda.includes('https://jkanime.net/')){
     await page.goto(idBusqueda);
   }else{
     await page.goto(`https://jkanime.net/buscar/${idBusqueda}/`);
@@ -66,13 +68,23 @@ const infoAnime = async(url)=>{
   const browser = await puppeteer.launch({headless:'new'})
   const page = await browser.newPage();
   await page.goto(url);
-  const result = await page.evaluate(async()=>{
+  const urlWeb = url;
+  const result = await page.evaluate(async(url)=>{
     const resultadoJSON = {};
+    const imagen =  document.querySelector('.anime__details__pic').getAttribute('data-setbg');
     const titulo =  document.querySelector('.anime__details__title h3').innerText;
     const resumen =  document.querySelector('p.tab.sinopsis').innerText;
     resultadoJSON.titulo = titulo
+    resultadoJSON.imagen = imagen
     resultadoJSON.resumen = resumen
-    
+    const paginacion = document.querySelectorAll('.anime__pagination a')
+    const paginas= [...paginacion].map((pagina)=>{
+      const referencia = url+pagina.getAttribute('href')
+      const titulo = pagina.innerText;
+      return {referencia,titulo}
+    })
+    resultadoJSON.paginacion = paginas
+
     const infoData =  document.querySelectorAll('.tab.aninfo li')
     const datos = [...infoData].map((data)=>{
       if(data.querySelector('li span').innerText === 'Genero:'){
@@ -103,36 +115,94 @@ const infoAnime = async(url)=>{
         resultadoJSON.emitido= emitido
       }
     })
+    return resultadoJSON
+},urlWeb)
+await browser.close();
+return result
+}
+
+const verAnime = async (idBusqueda) => {
+  const browser = await puppeteer.launch({headless:'new'})
+  const page = await browser.newPage();
+  await page.goto(idBusqueda);
   
-    // const existePrev = document.querySelector("a.text.nav-prev")
-    // const existeNext = document.querySelector("a.text.nav-next") 
-    // if (existeNext!= null) {
-    //   const enlace = existeNext.getAttribute('href')
-    //   resultadoJSON.navNext = enlace.replace(/ /g, '_');
-    // }
-    // if (existePrev!= null) {
-    //   const enlace = existePrev.getAttribute('href')
-    //   resultadoJSON.navPrev = enlace.replace(/ /g, '_');
-    // }
-    // const datos = [...items].map((item)=>{
-    //   const link = item.querySelector('a').getAttribute('href')
-    //   const imagen = item.querySelector('.anime__item__pic.set-bg').getAttribute('data-setbg')
-    //   const titulo = item.querySelector('.title').innerText;
-    //   const estado = item.querySelector('li').innerText;
-    //   const tipo = item.querySelector('li.anime').innerText;
-    //   return {
-    //     link,
-    //     imagen,
-    //     titulo,
-    //     estado,
-    //     tipo
-    //   }
-    // })
+  const result = await page.evaluate(async()=>{
+    const resultadoJSON = {};
+    const items =  document.querySelectorAll('#episodes-content .anime__item');
+
+    const datos = [...items].map((item)=>{
+      const link = item.querySelector('a').getAttribute('href')
+      const imagen = item.querySelector('.anime__item__pic.homemini.set-bg').getAttribute('data-setbg')
+      const titulo = item.querySelector('.anime__item__text span').innerText;
+      return {
+        link,
+        imagen,
+        titulo,
+      }
+    })
+    resultadoJSON.episodios = datos
     return resultadoJSON
 })
 await browser.close();
 return result
+};
+
+const verEpisodio = async (idBusqueda) => {
+  const browser = await puppeteer.launch({headless:'new'})
+  const page = await browser.newPage();
+  await page.goto(idBusqueda);
+  // Buscar el iframe por su clase "player_conte"
+  const iframeHandle = await page.$('.player_conte');
+  const result = await page.evaluate(async()=>{
+    const titulo =  document.querySelector('h1').innerText;
+    return titulo
+  })
+  if (iframeHandle) {
+  const iframe = await iframeHandle.contentFrame();
+  const videoElement = await iframe.$('video');
+  if (videoElement) {
+    // Hacer algo con la etiqueta video, por ejemplo, obtener su atributo src
+    const videoSrc = await iframe.evaluate(video => video.getAttribute('src'), videoElement);
+
+    await browser.close();
+    return {titulo:result,video:videoSrc}
+  } else {
+    const titulo= ('No se encontró la etiqueta <video> dentro del iframe.');
+    await browser.close();
+    return {titulo}
+  }
 }
+};
+
+
+const descargarEpisodio = async (urls) => {
+  const browser = await puppeteer.launch({headless:'new'})
+  const resultados = [];
+  for (const url of urls) {
+    const page = await browser.newPage();
+    await page.goto(url);
+    
+    // Buscar el iframe por su clase "player_conte"
+    const iframeHandle = await page.$('.player_conte');
+    const result = await page.evaluate(async()=>{
+      const titulo =  document.querySelector('h1').innerText;
+      return titulo
+    })
+    if (iframeHandle) {
+      const iframe = await iframeHandle.contentFrame();
+      await page.waitForSelector('video');
+      const videoElement = await iframe.$('video');
+      if (videoElement) {
+        // Hacer algo con la etiqueta video, por ejemplo, obtener su atributo src
+        const video = await iframe.evaluate(video => video.getAttribute('src'), videoElement);
+        resultados.push({result,video});
+        
+      } 
+    }
+  }
+  await browser.close();
+  return {resultados}
+};
 
 
 
@@ -143,9 +213,25 @@ expressApp.post('/api/buscar', async(req, res) => {
 });
 
 expressApp.post('/api/info', async(req, res) => {
-  const {valor} = req.body;infoAnime
+  const {valor} = req.body;
   const datos = await infoAnime(valor)
   res.json(datos);
+});
+expressApp.post('/api/ver', async(req, res) => {
+  const {valor} = req.body;
+  const datos = await verAnime(valor)
+  res.json(datos);
+});
+expressApp.post('/api/verEpisodio', async(req, res) => {
+  const {valor} = req.body;
+  const datos = await verEpisodio(valor)
+  res.json(datos);
+});
+expressApp.post('/api/descargar', async(req, res) => {
+  const {valor} = req.body;
+  const arrayData= await descargarEpisodio(valor)
+  console.log(arrayData)
+  res.json(arrayData);
 });
 
 app.whenReady().then(() => {
