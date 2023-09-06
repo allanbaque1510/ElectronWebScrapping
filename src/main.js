fa833ac9691a7e411aa27c47dae979c771389398
allanbaque1510 <allanbaque1510@gmail.com>
2023-09-06 07:15:37 -0500

    Actualizando programa

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const expressApp = express(); 
-
+expressApp.use(express.json());
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 900,
@@ -23,9 +23,24 @@ const coneccion = ()=>{
 const animeList = async (idBusqueda) => {
   const browser = await puppeteer.launch({headless:'new'})
   const page = await browser.newPage();
-  await page.goto(`https://jkanime.net/buscar/${idBusqueda}`);
-  const result = await page.evaluate(()=>{
+  if(idBusqueda.includes('https://jkanime.net/buscar/')){
+    await page.goto(idBusqueda);
+  }else{
+    await page.goto(`https://jkanime.net/buscar/${idBusqueda}/`);
+  }
+  const result = await page.evaluate(async()=>{
     const items =  document.querySelectorAll('.anime__item');
+    const resultadoJSON = {};
+    const existePrev = document.querySelector("a.text.nav-prev")
+    const existeNext = document.querySelector("a.text.nav-next") 
+    if (existeNext!= null) {
+      const enlace = existeNext.getAttribute('href')
+      resultadoJSON.navNext = enlace.replace(/ /g, '_');
+    }
+    if (existePrev!= null) {
+      const enlace = existePrev.getAttribute('href')
+      resultadoJSON.navPrev = enlace.replace(/ /g, '_');
+    }
     const datos = [...items].map((item)=>{
       const link = item.querySelector('a').getAttribute('href')
       const imagen = item.querySelector('.anime__item__pic.set-bg').getAttribute('data-setbg')
@@ -40,17 +55,96 @@ const animeList = async (idBusqueda) => {
         tipo
       }
     })
-    // const imagen = data.getAttribute('data-setbg')
-    return datos
+    resultadoJSON.Animes = datos
+    return resultadoJSON
 })
 await browser.close();
 return result
 };
 
+const infoAnime = async(url)=>{
+  const browser = await puppeteer.launch({headless:'new'})
+  const page = await browser.newPage();
+  await page.goto(url);
+  const result = await page.evaluate(async()=>{
+    const resultadoJSON = {};
+    const titulo =  document.querySelector('.anime__details__title h3').innerText;
+    const resumen =  document.querySelector('p.tab.sinopsis').innerText;
+    resultadoJSON.titulo = titulo
+    resultadoJSON.resumen = resumen
+    
+    const infoData =  document.querySelectorAll('.tab.aninfo li')
+    const datos = [...infoData].map((data)=>{
+      if(data.querySelector('li span').innerText === 'Genero:'){
+        const generos = data.querySelectorAll('a')
+        const generoData = [...generos].map((genero)=>{
+          const link = genero.getAttribute('href')
+          const tipo = genero.innerText
+          return {link,tipo}
+        })
+        resultadoJSON.genero = generoData
+      }
+      if(data.querySelector('li span').innerText === 'Idiomas:'){
+        const clon = data.cloneNode(true);
+        clon.querySelector('span').remove();
+        const idioma = clon.innerText.trim().replace(/  /g, '')
+        resultadoJSON.idioma= idioma
+      }
+      if(data.querySelector('li span').innerText === 'Episodios:'){
+        const clon = data.cloneNode(true);
+        clon.querySelector('span').remove();
+        const episodios = clon.innerText.trim().replace(/  /g, '')
+        resultadoJSON.episodios= episodios
+      }
+      if(data.querySelector('li span').innerText === 'Emitido:'){
+        const clon = data.cloneNode(true);
+        clon.querySelector('span').remove();
+        const emitido = clon.innerText.trim().replace(/  /g, '')
+        resultadoJSON.emitido= emitido
+      }
+    })
+  
+    // const existePrev = document.querySelector("a.text.nav-prev")
+    // const existeNext = document.querySelector("a.text.nav-next") 
+    // if (existeNext!= null) {
+    //   const enlace = existeNext.getAttribute('href')
+    //   resultadoJSON.navNext = enlace.replace(/ /g, '_');
+    // }
+    // if (existePrev!= null) {
+    //   const enlace = existePrev.getAttribute('href')
+    //   resultadoJSON.navPrev = enlace.replace(/ /g, '_');
+    // }
+    // const datos = [...items].map((item)=>{
+    //   const link = item.querySelector('a').getAttribute('href')
+    //   const imagen = item.querySelector('.anime__item__pic.set-bg').getAttribute('data-setbg')
+    //   const titulo = item.querySelector('.title').innerText;
+    //   const estado = item.querySelector('li').innerText;
+    //   const tipo = item.querySelector('li.anime').innerText;
+    //   return {
+    //     link,
+    //     imagen,
+    //     titulo,
+    //     estado,
+    //     tipo
+    //   }
+    // })
+    return resultadoJSON
+})
+await browser.close();
+return result
+}
 
-expressApp.get('/api/:id', async(req, res) => {
-  const parametro =  req.params.id;
-  const datos = await animeList(parametro)
+
+
+expressApp.post('/api/buscar', async(req, res) => {
+  const {valor} = req.body;
+  const datos = await animeList(valor)
+  res.json(datos);
+});
+
+expressApp.post('/api/info', async(req, res) => {
+  const {valor} = req.body;infoAnime
+  const datos = await infoAnime(valor)
   res.json(datos);
 });
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,7 +1,4 @@
-
-
 // formulario.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('miFormulario');
   
@@ -10,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
       const nombre = document.getElementById('nombre').value;
       // Aquí puedes hacer lo que desees con los datos, por ejemplo, enviarlos a través de IPC en Electron
       const formateado = nombre.trim();
-      console.log('Nombre:', formateado);
       prueba(formateado.replace(/ /g, '_'));
       // Limpia los campos del formulario si es necesario
       document.getElementById('nombre').value = '';
@@ -31,15 +27,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       })
     
-    fetch(`http://localhost:3000/api/${id}`)
+    fetch(`http://localhost:3000/api/buscar`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({valor:id}),
+    })
       .then((response) => response.json())
       .then((data) => {
         Swal.close();
         // Obtén una referencia al elemento <div> donde deseas mostrar los resultados
         const resultadosDiv = document.getElementById('resultados');
-        console.log(data)
+        const navegacionDiv = document.getElementById('navegacion');
         // Utiliza plantillas de JavaScript para construir el contenido HTML
-        const contenidoHTML = data.map((objeto) => {
+        const contenidoHTML = data.Animes.map((objeto) => {
           return `
             <div class='item' >
               <button class='tarjetaBtn' onclick="infoAnime('${objeto.link}')">
@@ -55,10 +57,15 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
           `;
         }).join(''); // Une las cadenas HTML en una sola
-  
-        // Inserta el contenido HTML en el elemento <div>
-        resultadosDiv.innerHTML = contenidoHTML;
-      })
+
+        const ambos =`
+        ${data.navPrev?`<button class='btnNav' onclick=prueba('${data.navPrev}')>Anterior</button>`:``}
+        ${data.navNext?`<button class='btnNav' onclick=prueba('${data.navNext}')>Siguiente</button>`:``}
+        `
+      navegacionDiv.innerHTML = ambos  
+      // Inserta el contenido HTML en el elemento <div>
+      resultadosDiv.innerHTML = contenidoHTML;
+    })
       .catch((error) => {
         console.log(error)
         Swal.fire({
@@ -68,3 +75,41 @@ document.addEventListener('DOMContentLoaded', () => {
         })
       });
   };
+  function infoAnime(url){
+    document.getElementById('listItems').classList.add('showInfoResult')
+    document.getElementById('informacion').classList.add('showInfo')
+    searchInfo(url)
+  }
+  async function searchInfo(url){
+
+    fetch(`http://localhost:3000/api/info`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({valor:url}),
+    }).then((response) => response.json())
+    .then((data) => {
+    
+      console.log(data) 
+      const divInfo = document.getElementById('informacion');
+      // Utiliza plantillas de JavaScript para construir el contenido HTML
+      const contenidoHTML =`
+          <div >
+          
+              <h3>${data.titulo}</h3>
+              <p>${data.resumen}</p>
+              <p >${data.emitido}</p>
+              <p>${data.episodios}</p>
+              ${data.genero.map((gen)=>{
+                `<a href='${gen.link}'>${gen.tipo}</a>`
+              })}
+       
+          </div>
+        `;
+        divInfo.innerHTML =contenidoHTML
+     
+
+    })
+
+  }
